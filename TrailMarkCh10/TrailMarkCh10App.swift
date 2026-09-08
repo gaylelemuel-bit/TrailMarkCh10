6caa042aca4081dcab629af6d6ee7691a9a75da8
gaylelemuel.bit <gaylelemuel@gmail.com>
2026-09-08 18:49:54 -0500

    Initial Commit

---
+++ TrailMarkCh10/TrailMarkCh10App.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMarkCh10App.swift
+//  TrailMarkCh10
+//
+//  Created by Lemuel Gayle on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMarkCh10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
